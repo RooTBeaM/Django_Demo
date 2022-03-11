6b479092445975def2d1a9121bfd11c8fd581726
zueyangzein <panya.sirikul@gamil.com>
2022-03-11 17:18:27 +0700

    ep 2 done

---
+++ myblog/blogapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Blog(models.Model): 
+    title = models.CharField(max_length=120) 
+    post = models.TextField() 
+    date_created = models.DateTimeField() 
+    date_updated = models.DateTimeField()
