a34365a1d0639b704f3b08ced8b7101547116d7c
zueyangzein <panya.sirikul@gamil.com>
2022-03-12 17:58:18 +0700

    ep7

--- myblog/blogapp/views.py
+++ myblog/blogapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 
 def home(request):
+    today = datetime.now()
+    return render(request, "blogapp/home.html", {'today':today})
 
-    return render(request, "blogapp/home.html")
+def about(request):
+
+    return render(request, "blogapp/about.html")
