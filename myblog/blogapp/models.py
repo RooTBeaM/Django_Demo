ca59c8f66e07d3251116abd49870868573476ad7
zueyangzein <panya.sirikul@gamil.com>
2022-03-13 17:37:05 +0700

    query&set dir media

--- myblog/blogapp/models.py
+++ myblog/blogapp/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 class Blog(models.Model): 
@@ -5,6 +6,12 @@ class Blog(models.Model):
     post = models.TextField() 
     date_created = models.DateTimeField() 
     date_updated = models.DateTimeField()
+
+    #link show image
+    #need python -m pip install Pillow
+    #need type python manage.py makemigrations
+    #save to database by python manage.py migrate
+    feild_img = models.ImageField(upload_to='featured_img', blank=True)
 
     def __str__(self):
         return self.title
