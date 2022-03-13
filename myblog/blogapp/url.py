8c27f39dd7fe2f607f76e945825448071491342c
zueyangzein <panya.sirikul@gamil.com>
2022-03-13 17:37:40 +0700

    query and media

--- myblog/blogapp/url.py
+++ myblog/blogapp/url.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
+    path('all-test', views.all_test, name='test'),
+    path('query-data', views.query_data, name='query'),
 ]
--- myblog/blogapp/views.py
+++ myblog/blogapp/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+
+from .models import Blog
 # Create your views here.
 
 def home(request):
     today = datetime.now()
-    return render(request, "blogapp/home.html", {'today':today})
+    posts = Blog.objects.all()
+    return render(request, "blogapp/home.html", {'today': today,
+                                                'posts':posts})
 
 def about(request):
 
     return render(request, "blogapp/about.html")
+
+def all_test(request):
+
+    return render(request, "blogapp/all_test.html")
+
+def query_data(request):
+    today = datetime.now()
+
+    posts = Blog.objects.all()
+    single_row = Blog.objects.get(pk = 3)
+    rows = Blog.objects.all()[1:3]
+    reversed_posts = Blog.objects.order_by('-id')
+    columns = Blog.objects.values('title', 'id')
+    first_row = Blog.objects.first()
+    last_row = Blog.objects.last()
+
+
+    return render(request, "blogapp/query_data.html", {'today': today, 
+                                                'posts' : posts, 
+                                                'single_row': single_row,
+                                                'rows': rows,
+                                                'reversed_posts' : reversed_posts,
+                                                'columns': columns,
+                                                'first_row': first_row,
+                                                'last_row': last_row
+                                                })
